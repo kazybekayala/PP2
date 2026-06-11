e4fdeeed0a1b0eca7bca8ed7ba583a94564ea59c
Ayala <kazybek.ayala07@gmail.com>
2026-06-11 10:39:04 +0500

    Add Practice1 Python basics examples

---
+++ practice_1/data_types.py
@@ -0,0 +1,26 @@
+x = 5
+print(type(x))
+
+x = 20
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+
+x = None
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+
+x = float(20.5)
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+
+x = bool(5)
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
